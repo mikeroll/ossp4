e19e899f8766be224ee893253c64d781e452be4f
Mikhail Bulash <scaryspiderpig@gmail.com>
2014-03-30 22:34:32 +0300

    Input file as enumerable

---
+++ MapReduce/Workload.cs
@@ -0,0 +1,65 @@
+using System;
+using System.IO.MemoryMappedFiles;
+using System.IO;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace MapReduce
+{
+    /// <summary>
+    /// Represents the data to be processed by MapReduce
+    /// </summary>
+    public class Workload : IEnumerable<string>, IDisposable
+    {
+        public string Filename { get; private set; }
+        public long Size { get; private set; }
+
+        FileStream stream;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MapReduce.Workload"/> class.
+        /// </summary>
+        /// <param name="filename">Data file name.</param>
+        public Workload(string filename)
+        {
+            Filename = filename;
+            Size = new FileInfo(filename).Length;
+            stream = File.OpenRead(filename);
+            if (!stream.CanRead)
+            {
+                throw new IOException("Input file provided can not be read.");
+            }
+        }
+
+        #region IEnumerable implementation
+
+        //FIXME: Very long lines will break everything. No fix for now, major redesign needed.
+        public IEnumerator<string> GetEnumerator()
+        {
+            using (var sr = new StreamReader(stream))
+            {
+                while (!sr.EndOfStream) 
+                {
+                    yield return sr.ReadLine();
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
+
+        #region IDisposable implementation
+
+        public void Dispose()
+        {
+            stream.Dispose();
+        }
+
+        #endregion
+    }
+}
+
--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -3,6 +3,7 @@ using System;
 using System.Runtime.InteropServices;
 
 using MapReduce;
+using System.Threading.Tasks;
 
 namespace Tests
 {
@@ -15,6 +16,45 @@ namespace Tests
             const int expected = 3;
             int actual = NativeStringTools.Z("lal","lalalalalalal");
             Assert.AreEqual(expected, actual);
+        }
+    }
+
+    [TestFixture]
+    public class WorkloadTests
+    {
+        static string dmesg = @"/tmp/file";
+
+        [Test]
+        public void WorkloadInit()
+        {
+            using (var w = new Workload(dmesg))
+            {
+                //something
+            }
+        }
+
+        [Test]
+        public void WorkloadEnumerate()
+        {
+            using (var w = new Workload(dmesg))
+            {
+                foreach (var line in w)
+                {
+                    NativeStringTools.Z("kernel", line);
+                }
+            }
+        }
+
+        [Test]
+        public void WorkloadParallelEnumerate()
+        {
+            using (var w = new Workload(dmesg))
+            {
+                // DANGER: not memory-efficient at all (?)
+                Parallel.ForEach(w, (line) => {
+                    NativeStringTools.Z("kernel", line);
+                });
+            }
         }
     }
 }
